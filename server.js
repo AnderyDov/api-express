069dd57eb840eebcafd6ec0c118c337d91789775
Andrey <dovban.andreyy@rambler.ru>
2023-02-10 16:38:26 +0300

    добавлен error 404

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const port = '3000';
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log(Date.now());
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('Home page');
 });
@@ -15,6 +20,10 @@ app.get('/hello', (req, res) => {
 });
 
 app.use('/users', usersRouter);
+
+app.use((req, res) => {
+    res.send('Error 404');
+});
 
 app.listen(port, () => {
     console.log(`http://${host}:${port}`);
--- users/users.js
+++ users/users.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 const usersRouter = express.Router();
 
+usersRouter.use((req, res, next) => {
+    console.log('middlewar users');
+    next();
+});
+
 usersRouter.get('/login', (req, res) => {
     res.send('login');
 });
